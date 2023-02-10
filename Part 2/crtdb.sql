e95168310b5f6f5756efd671cac061f9119d4070
jespana104 <jespana104@gmail.com>
2023-02-10 08:39:21 -0700

    Update crtdb.sql
    Updates

--- Part 2/crtdb.sql
+++ Part 2/crtdb.sql
@@ -4,38 +4,38 @@
 
 --Creates by Joshua
 create table Broadcasts(
-    videoCode: integer, 
-    siteCode: integer
+    videoCode integer, 
+    siteCode integer,
     Foreign key (videoCode) references Video(videoCode),
     Foreign key (siteCode) references Site (siteCode)
 );
 
 create table Administers(
-    empId: integer, 
-    siteCode: integer,
+    empId integer, 
+    siteCode integer,
     Foreign key (empId) references Administrator (empId),
     Foreign key (siteCode) references Site (siteCode)
 );
 
 create table Specializes(
-    empId: integer, 
-    modelNo: char(10),
+    empId integer, 
+    modelNo text,
     Foreign key (empId) references TechnicalSupport (empId),
     Foreign key (modelNo) references Model (modelNo)
 );
 
 create table Purchases(
-    clientId: integer, 
-    empId: integer, 
-    packageId: integer,
-    commissionRate: numeric
+    clientId integer, 
+    empId integer, 
+    packageId integer,
+    commissionRate numeric,
     Foreign key (empId) references Salesman (empId),
     Foreign key (packageId) references AirtimePackage (packageId)
 );
 
 create table Locates(
-    serialNo: text, 
-    siteCode: integer,
+    serialNo text, 
+    siteCode integer,
     Foreign key (serialNo) references DigitalDisplay (serialNo) ,
     Foreign key (siteCode) references Site (siteCode) 
 );
