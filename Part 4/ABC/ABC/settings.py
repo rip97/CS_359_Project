5301ecb1d2f8d57aad66dac2352ca897ca94dbb7
rip97 <jrippie@outlook.com>
2023-04-11 21:26:21 -0600

    Basic Login Page

--- Part 4/ABC/ABC/settings.py
+++ Part 4/ABC/ABC/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ABC_Media.apps.AbcMediaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Part 4/ABC/ABC_Media/views.py
+++ Part 4/ABC/ABC_Media/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+
+def login(request):
+    template = loader.get_template("ABC_Media/login.html")
+    return render(request, "ABC_Media/login.html")
