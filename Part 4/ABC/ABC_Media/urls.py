0c2094d3df7837e0800d7fa71ecd17e7351a9f98
jespana104 <jespana104@gmail.com>
2023-04-25 20:40:58 -0600

    Revert "Database management and create view created"

--- Part 4/ABC/ABC_Media/urls.py
+++ Part 4/ABC/ABC_Media/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("login/", views.login, name="login"),
     path("main/", views.main, name="main"),
-    path("create/", views.create, name="create"),
     path("logout/", views.logout, name="logout"),
     path("search_digi_disp/", views.search_digi_disp,name="search")
 ]
--- Part 4/ABC/ABC_Media/views.py
+++ Part 4/ABC/ABC_Media/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from .forms import DatabaseInputForm
 from .forms import SearchForm
-from .forms import DigitalDisplayForm
-from pathlib import Path
-import os
-from ABC_Media import database_manager
 from django.db import connection
 from django.contrib import messages
 from django.http import HttpResponse
@@ -61,33 +57,6 @@ def main(request):
             return render(request, "ABC_Media/main.html", param)
     else:
         return redirect("ABC_Media:login")
-    
-# Joshua Espana
-# Create controller
-def create(request):
-
-    if 'username' in request.session:
-        current_user = request.session['username']
-        param = {'current_user': current_user, 'validation_status': '' }
-        if request.method == "POST":
-            # Places form results into object for easier calling
-            dd = {"serailNo": request.POST["serialNumber"], "schSys": request.POST["schedulerSystem"], "modelNo": request.POST["modelNo"]}
-            # Connects to database
-            dbX = database_manager.database_manager(os.path.join(Path.cwd(),"ABC",current_user))
-            # Checks serail number for uniquess
-            if(dbX.isSerialNumberUnique(request.POST["serialNumber"]) == False):
-                param['validation_status'] = 'Serail number already in use'
-                return render(request, "ABC_Media/create.html", param)
-            # Checks all inputs for completeness
-            if(dd["serailNo"] == "" or dd["schSys"] == "" or dd["modelNo"] == ""):
-                param['validation_status'] = 'Please check all input values for completeness'
-                return render(request, "ABC_Media/create.html", param)
-            dbX.create(dd["serailNo"],dd["schSys"], dd["modelNo"])
-            return redirect("ABC_Media:main")
-    else:
-        return redirect("ABC_Media:login")
-
-    return render(request, "ABC_Media/create.html", param)
 
 
 # need to work this method out - Rippie
